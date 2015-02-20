379262c22e2c66dd4a874d57af118184400aadb5
Michael Miller <pfxmichaelm@gmail.com>
2015-02-20 10:47:18 -0700

    getGuess input

--- js/app.js
+++ js/app.js
@@ -12,27 +12,35 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	var answer = Math.floor((Math.random() * 100) + 1);
+    console.log('Secret number is: ' + answer);
+    var numberOfGuess = 0
+    var guesses = [];
+    var distance = null;
+    var prevDistance = null;
 });
+
+function getGuess() {
+  $(document).ready(function() {
+    $('#guessButton').click(function() {
+      var uGuess = $('#userGuess').val();
+      uGuess = +uGuess;
+      console.log(typeof uGuess)
+      console.log('Guess: ' + uGuess);	
+    });
+    //game();
+  });
+}
+
+getGuess();
 
 function newGame() {
 
 }
 
-function secretNum() {
-  num = Math.floor(Math.random() * 100);
-  console.log(typeof num);
+function game() {
+
 }
 
-secretNum();
-console.log(num);
 
-function guess() {
-  if (#userGuess == num) {
-  	alert("Correct!!");
-  }
-  else {
-
-  }
-}
-
-$('#userGuess').focus();
+//$('#userGuess').focus();
