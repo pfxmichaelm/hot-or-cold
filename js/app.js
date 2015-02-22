bdddd288f0f2b177cd2fb57613fc482304a18bdb
Michael Miller <pfxmichaelm@gmail.com>
2015-02-22 08:36:02 -0700

    more logic

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
   var answer = Math.floor((Math.random() * 100) + 1);
   console.log('number is: ' + answer);
   answer = Math.floor((Math.random() * 100) + 1);
-  var uGuess = 0;
+  //var uGuess = 0;
   var numOfGuess = 0;
   var guess = [];
   var distance = null;
@@ -41,25 +41,34 @@ $(document).ready(function() {
 
   function game() {
     console.log('enter game' + ', num = ' + answer);
-    distance = Math.abs(answer - uGuess);
-    console.log('dist = ' + distance);
-    if (distance > 50) {
-      console.log('ice cold');
+    var uGuess = parseInt($('#userGuess').val());
+    if (uGuess != null && $.isNumeric(uGuess) && (1 < uGuess < 101)) {
+      numOfGuess += 1;
+      guess.push(uGuess);
+      distance = Math.abs(answer - uGuess);
+      console.log('dist = ' + distance);
+      if (uGuess == answer) {
+        console.log("Winner");
+      }
+      else {
+        if (distance > 50) {
+          console.log('ice cold');
+        }
+        else if (distance < 50 && distance > 30) {
+          console.log('cold');
+        }
+        else if (distance < 30 && distance > 20) {
+          console.log('warm');
+        }
+        else if (distance < 20 && distance > 10) {
+          console.log('hot');
+        }
+        else if (distance < 10 && distance > 1) {
+          console.log('very hot');
+        }
+        $('#userGuess').val(' ');
+      }
     }
-    else if (distance < 50 && distance > 30) {
-      console.log('cold');
-    }
-    else if (distance < 30 && distance > 20) {
-      console.log('warm');
-    }
-    else if (distance < 20 && distance > 10) {
-      console.log('hot');
-    }
-    else if (distance < 10 && distance > 1) {
-      console.log('very hot');
-    }
-    $('#userGuess').val(' ');
-
   }
 
   $('.new').click(function(event) {
